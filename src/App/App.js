10787d8b96d6ef28b426b6c4d2606dcb7ea5c1a3
EthanDuvall <Eduvall9405@gmail.com>
2024-08-12 00:49:41 -0600

    Create header

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import Home from '../Home/Home'
 
 function App() {
   return (
    <>
-   
+    <header>
+      <h1>Co-op Finder</h1>
+      <form>
+        <h2>Please enter your uid and display name before starting</h2>
+        <input type = 'number' placeholder='UID'></input>
+        <input type = 'text' placeholder='Display Name'></input>
+        <button>Confirm</button>
+      </form>
+    </header>
+    <body>
+      <Home/>
+    </body>
    </>
   );
 }
---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+
+
+
+function Home(){
+
+
+
+}
+
+
+export default Home
