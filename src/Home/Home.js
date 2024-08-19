640cc037152db907493d13baf4e79bfebe15ee46
EthanDuvall <Eduvall9405@gmail.com>
2024-08-18 19:29:32 -0600

    Create filter function

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,40 +1,56 @@
-import { useState } from "react"
-import Lobbies from "../Lobbies/Lobbies"
+import { useState } from "react";
+import Lobbies from "../Lobbies/Lobbies";
+
+function Home() {
+
+  const [filters, setFilters] = useState([]);
+  const [search, setSearch] = useState("");
+  const [lobbies, setLobbies] = useState([]);
+  const [filteredLobbies, setFilteredLobbies] = useState([]);
 
 
-function Home(){
 
-    const [filters, setFilters] = useState([])
-    const [search, setSearch] = useState([])
 
-    function dynamicSearch(){
+  function dynamicSearch() {}
 
+
+
+  function filterLobbies() {
+    setFilteredLobbies(
+      lobbies.reduce((newLobbies, lobby) => {
+        filters.forEach((filter) => {
+          if (lobby.tags.contains(filter)) {
+            newLobbies.push(lobby);
+          }
+        });
+      }, [])
+    );
+  }
+
+  function activateFilter(tag){
+    if(filters.contains(tag)){
+        setFilters(filters.filter((filter) => filter =! tag))
+    }else{
+        setFilters([...filters, tag])
     }
-    function filterLobbies(){
+  }
 
-
-    }
-    
-
-    return(
+  return (
     <>
-    <div className="filter-bar">
-        <button>Create Lobby</button>
-        <button>Friendship</button>
-        <button>World Bosses</button>
-        <button>Kath / Dailes</button>
-        <button>Domains</button>
-        <button>Weekly Bosses</button>
-        <input placeholder="Search..." type="text" ></input>
-    </div>
-    <div className="lobby-holder">
-        <Lobbies/>
-    </div>
-
-    </>)
-
-
+      <div className="filter-bar">
+        <button >Create Lobby</button>
+        <button onClick={()=>{activateFilter(friendship)}}>Friendship</button>
+        <button onClick={()=>{activateFilter(worldBoss)}}>World Bosses</button>
+        <button onClick={()=>{activateFilter(kath)}}>Kath / Dailes</button>
+        <button onClick={()=>{activateFilter(domain)}}>Domains</button>
+        <button onClick={()=>{activateFilter(weeklyBoss)}}>Weekly Bosses</button>
+        <input placeholder="Search..." type="text"></input>
+      </div>
+      <div className="lobby-holder">
+        <Lobbies />
+      </div>
+    </>
+  );
 }
 
-
-export default Home
+export default Home;
